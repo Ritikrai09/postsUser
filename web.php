b88b76a4f3dd3296af1549145e4bce33a9a4af09
Ritikrai09 <62825561+Ritikrai09@users.noreply.github.com>
2021-12-26 23:17:19 +0530

    Update web.php

--- web.php
+++ web.php
@@ -36,25 +36,3 @@ Route::delete('/posts/{post}', [PostController::class,'destroy'])->name('posts.d
 
 Route::post('/posts/{post:id}/likes', [LikeController::class,'store'])->name('posts.likes');
 Route::delete('/posts/{post:id}/likes', [LikeController::class,'destroy'])->name('posts.likes');
-
-Route::get('/seller', [SellerController::class,'index'])->name('seller');
-
-Route::get('/product', function () {
-    return view('layouts.index');
-});
-
-///-------------------------------------------------------------------ecommerce----------------------------
-
-Route::get('/seller/dashboard',function(){return view('layouts.selldash');})->name('sellerdashboard');
-Route::get('/orders',function(){return view('layouts.ecommerce.order');})->name('orders');
-Route::get('/products',function(){ return view('layouts.ecommerce.product');})->name('products');
-Route::post('/products', [ProductController::class,'store']);
-Route::get('/customers',function(){ return view('layouts.ecommerce.customer');})->name('customers');
-Route::get('/report',function(){ return view('layouts.ecommerce.report');})->name('report');
-Route::get('/billings',function(){ return view('layouts.ecommerce.bill');})->name('billings');
-
-Route::prefix('seller')->group(function(){
-Route::get('/register',function(){ return view('layouts.ecommerce.sellerRegister');})->name('sellerRegister');
-Route::post('/register',[RegisterController::class,'store']);
-Route::get('/login',function(){ return view('layouts.ecommerce.sellerLogin');})->name('sellerlogin');
-});
