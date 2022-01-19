3d2fa03ee9d38872dbaf00ea643007565a95177f
Ritikrai09 <62825561+Ritikrai09@users.noreply.github.com>
2022-01-19 13:36:06 +0530

    Delete layouts directory

--- layouts/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('auth.app')
-
-@section('content1')
-<div class='flex justify-center'> 
-        <div class="w-2/6 bg-white p-6 rounded-lg mt-3">
-          Home
-    </div>
-</div>
-@endsection
--- layouts/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('auth.app')
-
-@section('content1')
-<div class='flex justify-center'> 
-        <div class="w-2/6 bg-white p-6 rounded-lg mt-3">
-        <x-post :post='$post'/>
-    </div>
-</div>
-@endsection
